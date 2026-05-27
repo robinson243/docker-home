9f99a7d611ad3375a73f133231b67f73eaf4b9e6
mukena_r <robinsonmukena@gmail.com>
2026-05-27 17:22:42 +0200

    start mariadb

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM celtak/ubuntu-ping-ip
+RUN apt update && apt install -y nodejs 
---
+++ php/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+const DBHOST = 'db';
+const DBUSER = 'test';
+const DBPASS = 'pass';
+const DBNAME = 'demo'
+
+$dsn = 'mysql:host=' . DBHOST . ';dbname=' . DBNAME;

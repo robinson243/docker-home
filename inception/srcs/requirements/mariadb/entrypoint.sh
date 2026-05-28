83709f95d839b722d9bd1491b001e2d9a36bf110
mukena_r <robinsonmukena@gmail.com>
2026-05-28 20:22:29 +0200

    add mariadb script stuff

--- inception/srcs/requirements/mariadb/entrypoint.sh
+++ inception/srcs/requirements/mariadb/entrypoint.sh
@@ -6,4 +6,14 @@ fi
 
 mysqld --user=mysql --skip-networking &
 
-exec mysqld --user=mysql
+until mysqladmin ping --silent; do sleep 1; done
+
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysqladmin shutdown
+
+exec mysqld --user=mysql --datadir=/var/lib/mysql

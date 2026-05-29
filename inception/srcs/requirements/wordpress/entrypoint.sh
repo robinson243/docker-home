a98336ec74ae0d8a85a254229571259f98f0c398
mukena_r <robinsonmukena@gmail.com>
2026-05-29 18:19:59 +0200

    wordpress part

---
+++ inception/srcs/requirements/wordpress/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+until mysqladmin ping -h mariadb --silent; do
+    sleep 1
+done
+
+if [ ! -d "/var/www/html/wp-admin" ]; then
+
+    wp core download --allow-root --path=/var/www/html
+
+    wp config create \
+        --allow-root \
+        --path=/var/www/html \
+        --dbname=$DB_NAME \
+        --dbuser=$DB_USER \
+        --dbpass=$DB_PASSWORD \
+        --dbhost=mariadb
+
+    wp core install \
+        --allow-root \
+        --path=/var/www/html \
+        --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --skip-email
+
+fi
+
+exec php-fpm7.4 -F
